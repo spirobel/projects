7cb8ee935c247b3d82ee62d6c11157ef6fd15081
Spiro Spirobel <spirobel@protonmail.com>
2019-09-26 14:05:36 +0200

    add category setting

--- plugin.rb
+++ plugin.rb
@@ -16,4 +16,20 @@ load File.expand_path('../lib/projects/engine.rb', __FILE__)
 
 after_initialize do
   # https://github.com/discourse/discourse/blob/master/lib/plugin/instance.rb
+  Category.register_custom_field_type('projects_enabled', :boolean)
+  [
+    "projects_enabled"
+  ].each do |key|
+    Site.preloaded_category_custom_fields << key if Site.respond_to? :preloaded_category_custom_fields
+    add_to_serializer(:basic_category, key.to_sym) { object.send(key) }
+  end
+  class ::Category
+    def projects_enabled
+      if self.custom_fields['projects_enabled'] != nil
+        self.custom_fields['projects_enabled']
+      else
+        false
+      end
+    end
+  end
 end
