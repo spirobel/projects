e50bc35955630ba1e18c7e6c518e54448d30216c
spirobel <spirobel@protonmail.com>
2020-01-30 11:22:12 +0100

    move route into engine

---
+++ app/controllers/project/notes_controller.rb
@@ -0,0 +1,17 @@
+module Project
+  class NotesController < ApplicationController
+    def update
+      Rails.logger.info 'Called NotesController#update'
+
+      note_id = params[:note_id]
+      note = {
+        'id' => note_id,
+        'date' => params[:note][:date]
+      }
+
+      puts note_id, note
+
+      render json: { note: note }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Project::Engine.routes.draw do
   get "/" => "projects#index", constraints: ProjectConstraint.new
   get "/actions" => "actions#index", constraints: ProjectConstraint.new
   get "/actions/:id" => "actions#show", constraints: ProjectConstraint.new
+  put "/notes/:note_id" => "notes#update"
 end
